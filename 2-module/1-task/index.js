ef4f598ce508a1129314025bc0d0adc2dbb5a038
Alex <cikrf.ru@mail.ru>
2018-10-09 15:15:19 +0300

    в одну ветку всё

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,13 @@
  * @returns {Object}
  */
 function clone (obj) {
+let clones={};
+
+function clone (obj){
+      
+    for (let key in obj){
+        clones[key] = obj[key];
+    }
+    return clones;
+} 
 }
